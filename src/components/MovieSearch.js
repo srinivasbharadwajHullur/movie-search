e74605d286ae55186b951e0b963bca823d9b9e7c
srinivasbharadwaj8 <srinivasbharadwaj8@gmail.com>
2023-02-11 12:13:55 +0530

    Added the spinner icon

--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -2,17 +2,30 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Display from './Display';
 import Form from 'react-bootstrap/Form';
+import Spinner from 'react-bootstrap/Spinner';
+import { Button } from 'react-bootstrap';
 
 
 const MovieSearch = () => {
     const [search, setSearch] = useState('')
-    const [movieResult, setMovieResult] = useState([])
+    const [movieResult, setMovieResult] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
     const fetchMovieDetails = async() => {
+        setLoading(true)
         axios.get(`https://www.omdbapi.com/?apikey=eb2021cf&s=${search}&type=movie`)
         .then((response) => {
             if(response.data.Response === "True") {
                 setMovieResult(response.data.Search)
+                setLoading(false)
             }
+        })
+        .catch((error) => {
+            setErrorMessage("Unable to fetch the movie list")
+            setLoading(false)
+        })
+        .finally(() => {
+            setLoading(false)
         })
     }
 
@@ -31,7 +44,8 @@ const MovieSearch = () => {
                 />
             </Form.Group>
         </Form>
-        <Display movieResult={movieResult}/>
+        {loading ? <Spinner animation="grow" /> : <Display movieResult={movieResult}/>}
+        {errorMessage && <div className='error'>{errorMessage}</div> }
     </div>
   )
 }
